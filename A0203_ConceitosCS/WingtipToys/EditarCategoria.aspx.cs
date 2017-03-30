09a490a4ef530fcee9c38f52bd38374a17174aa4
junior <junior@WALDEMARJUN61E9>
2017-03-30 11:33:52 -0300

    b

---
+++ A0203_ConceitosCS/WingtipToys/EditarCategoria.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WingtipToys.Models;
+
+namespace WingtipToys
+{
+    public partial class EditarCategoria : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btnCadastrar_Click(object sender, EventArgs e)
+        {
+
+            using (ContextoProduto contexto = new ContextoProduto())
+            {
+                Categoria c = new Categoria();
+
+                c.Nome = txtNome.Text;
+                c.Descricao = txtDescricao.Text;
+                
+                contexto.Categorias.Add(c);
+                contexto.SaveChanges();
+                Response.Redirect("/EditarCategoria.aspx");
+            }
+        }
+    }
+}
